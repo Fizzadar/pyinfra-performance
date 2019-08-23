49dbb33717e9ba9f75dc48cd5c95540577b72b3e
Nick Barrett <pointlessrambler@gmail.com>
2019-08-23 18:52:11 +0100

    Tidy up pyinfra deploy.

--- tests/deploy/deploy.py
+++ tests/deploy/deploy.py
@@ -2,22 +2,30 @@
 # File: deploy/deploy.py
 # Desc: the pyinfra deploy!
 
-from pyinfra.modules import server, files
+from pyinfra.modules import files, server
 
-SUDO = True
-TIMEOUT = 1
+CONNECT_TIMEOUT = 1
 FAIL_PERCENT = 0
 
-# Add pyinfra user
-server.user('pyinfra', home='/home/pyinfra', shell='/bin/bash')
-
-# Add log file
-files.file('/var/log/pyinfra.log', user='pyinfra', mode=777)
-
-# Copy a file
-files.put('../files/test_file.txt', '/home/pyinfra/test_file.txt', user='pyinfra')
-
+server.user(
+    {'Add pyinfra user'},
+    'pyinfra',
+    home='/home/pyinfra', shell='/bin/bash',
 )
 
-# Run some shell
-server.shell('echo "hi!"')
+files.file(
+    {'Add log file'},
+    '/var/log/pyinfra.log',
+    user='pyinfra', mode=777,
+)
+
+files.put(
+    {'Copy a file'},
+    '../files/test_file.txt', '/home/pyinfra/test_file.txt',
+    user='pyinfra',
+)
+
+server.shell(
+    {'Run some shell'},
+    'echo "hi!"',
+)
