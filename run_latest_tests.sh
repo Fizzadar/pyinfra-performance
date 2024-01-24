a71ee69f42947808c20976beef5f58f5db34ee84
Nick Mills-Barrett <nick@fizzadar.com>
2024-01-24 20:13:45 +0000

    Fix & cleanup test CLI

--- run_latest_tests.sh
+++ run_latest_tests.sh
@@ -5,16 +5,11 @@ set -e
 echo "--> Reset results.csv"
 echo "test,hosts,time" > results.csv
 
-PYINFRA_TEST_HOSTS=1 ./run_tests.sh
-PYINFRA_TEST_HOSTS=5 ./run_tests.sh
-PYINFRA_TEST_HOSTS=25 ./run_tests.sh
-PYINFRA_TEST_HOSTS=50 ./run_tests.sh
-PYINFRA_TEST_HOSTS=100 ./run_tests.sh
-PYINFRA_TEST_HOSTS=150 ./run_tests.sh
-PYINFRA_TEST_HOSTS=200 ./run_tests.sh
-PYINFRA_TEST_HOSTS=250 ./run_tests.sh
-PYINFRA_TEST_HOSTS=300 ./run_tests.sh
-PYINFRA_TEST_HOSTS=350 ./run_tests.sh
-PYINFRA_TEST_HOSTS=400 ./run_tests.sh
-PYINFRA_TEST_HOSTS=450 ./run_tests.sh
-PYINFRA_TEST_HOSTS=500 ./run_tests.sh
+./run_tests.sh -t 1
+./run_tests.sh -t 5
+./run_tests.sh -t 25
+./run_tests.sh -t 50
+./run_tests.sh -t 100
+./run_tests.sh -t 250
+./run_tests.sh -t 500
+./run_tests.sh -t 1000
--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,16 @@
 
 set -e
 
-bold=`tput bold`
-normal=`tput sgr0`
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# Use gdate if installed (brew install coreutils) for nanosecond accuracy
+DATE=$(which date)
+if command -v gdate > /dev/null 2>&1; then
+    DATE=$(which gdate)
+fi
+
+export TIMEFORMAT=%R
 
 # Define our tests to time
 declare -A TESTS=(
@@ -20,18 +28,58 @@ declare -A TESTS=(
 # Forces Ansible to accept all host keys
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-# Work out if printing output
 VERBOSE="false"
-if [ "${1}" = "-v" ]; then
-    VERBOSE="true"
-fi
+POSITIONAL_ARGS=()
+TEST_SIZE=5
+PRE_SLEEP=15
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v)
+        VERBOSE="true"
+        shift # flag
+        ;;
+    -t)
+        TEST_SIZE=$2
+        shift # flag
+        shift # value
+        ;;
+    --pre-sleep)
+        PRE_SLEEP=$2
+        shift # flag
+        shift # value
+        ;;
+    -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # argument
+        ;;
+  esac
+done
+
+export PYINFRA_TEST_HOSTS=$TEST_SIZE
+
+
+array_contains () {
+    local seeking=$1; shift
+    local in=1
+    for element; do
+        if [[ $element == "$seeking" ]]; then
+            in=0
+            break
+        fi
+    done
+    return $in
+}
 
 function start_containers() {
     echo "Starting ${1} containers"
 
     for i in `seq 1 ${1}`; do
-        docker run -d -p $((8999 + i)):22 pyinfra-performance-sshd >/dev/null &
+        docker run -d -p $((18999 + i)):22 pyinfra-performance-sshd > /dev/null &
 
         if ! (( $i % 50 )); then
             echo "${i}/${1}"
@@ -42,83 +90,85 @@ function start_containers() {
     wait
 }
 
-
 function kill_containers() {
-    local containers=`docker ps -q`
+    local containers=$(docker ps -q)
 
     if [ ! "$containers" = "" ]; then
         docker kill $containers > /dev/null
+        docker rm $containers > /dev/null
     fi
 }
 
-
 function run_test() {
-    START=`date +%s.%N`
+    local test_name=$1
 
-    # Are we printing output?
+    echo "Run test $test_name command: ${@:2}"
+
+    START=$($DATE +%s.%N)
+
+    set +e
     if [ "${VERBOSE}" = "true" ]; then
         ${@:2}
     else
-        ${@:2} > /dev/null
+        ${@:2} > /dev/null 2>&1
     fi
-
-    # Flag whether failed or not
     if [ ! "${?}" = "0" ]; then
         FAILED="true"
     else
         FAILED="false"
     fi
+    set -e
 
-    local END=`date +%s.%N`
-    local DIFF=`echo "$END - $START" | bc`
+    local END=$($DATE +%s.%N)
+    local DIFF=$(echo "$END - $START" | bc)
 
     if [ "${FAILED}" = "true" ]; then
         echo "--> ${1} failed after ${DIFF} seconds"
-        return 1
+    else
+        echo "--> ${test_name} complete in ${DIFF} seconds"
     fi
 
-    local test_name=$1
-
-    echo "--> ${test_name} complete in ${DIFF} seconds"
-    echo "${test_name},${n_hosts},${DIFF}" >> results.csv
+    echo "${test_name},${TEST_SIZE},${DIFF}" >> results.csv
     return
 }
+
+function cleanup() {
+    echo "--> Cleaning up remaining containers..."
+    kill_containers
+}
+trap cleanup EXIT
 
 
 # Let's go!
 echo "### ${bold}pyinfra Performance Tests${normal}"
 
 # Work out the number of hosts (5 is default)
-n_hosts=`echo $PYINFRA_TEST_HOSTS`
-if [ "${n_hosts}" = "" ]; then
-    n_hosts="5"
-fi
-echo "--> Running with ${n_hosts} hosts"
+echo "--> Running with ${TEST_SIZE} hosts"
 
 echo "--> ssh-add the key"
-ssh-add ./docker/performance_rsa > /dev/null 2>&1
+ssh-add ./docker/performance_rsa
 
 echo "--> Building docker SSH image..."
-docker build -t pyinfra-performance-sshd -f docker/Dockerfile ./docker > /dev/null
+docker build -t pyinfra-performance-sshd -f docker/Dockerfile ./docker
 
 # Run each test
 for TEST in "${!TESTS[@]}"; do
-    # Remove any existihg containers
+    if [ ! "${POSITIONAL_ARGS}" = "" ]; then
+        array_contains "$TEST" "${POSITIONAL_ARGS[@]}" || continue
+    fi
+
     echo "--> Removing any containers..."
     kill_containers
 
-    # Up new containers
-    echo "--> Spawning ${n_hosts} containers..."
-    start_containers ${n_hosts}
+    echo "--> Spawning ${TEST_SIZE} containers..."
+    start_containers ${TEST_SIZE}
 
-    sleep 5
+    echo "--> Sleep ${PRE_SLEEP}s..."
+    sleep ${PRE_SLEEP}
 
-    # Do the tests
-    time run_test ${TEST} ${TESTS[$TEST]}
+    echo "--> Run test: ${TEST}"
+    run_test ${TEST} ${TESTS[$TEST]}
     echo
 done
-
-echo "--> Cleaning up remaining containers..."
-kill_containers
 
 echo "<-- All tests complete!"
